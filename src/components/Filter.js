84af0c4161ef104b10c27e9c39b35ac54dfd609f
Kelvin Gil <gilkelvin2@gmail.com>
2024-04-15 19:47:17 -0400

    done

---
+++ src/components/Filter.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+
+
+function Filter({onCategoryChange}){
+    return (
+        <div className="Filter">
+            <select name="filter" onChange={onCategoryChange}>
+                <option value="All">Filter by category</option>
+                <option value="Produce">Produce</option>
+                <option value="Dairy">Dairy</option>
+                <option value="Dessert">Dessert</option>
+            </select>
+        </div> 
+    )
+}
+
+
+
+{/* <div className="Filter">
+<select name="filter" onChange={handleCategoryChange}>
+  <option value="All">Filter by category</option>
+  <option value="Produce">Produce</option>
+  <option value="Dairy">Dairy</option>
+  <option value="Dessert">Dessert</option>
+</select>
+</div> */}
+
+
+
+export default Filter
